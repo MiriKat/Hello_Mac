40bcfe793dce94e8afdc6a4799ba34af57530d42
Maryam.Khatoon <maryam.khatoon@focusrite.com>
2016-08-11 22:42:51 +0100

    1st exercise.

--- Hello_Mac/ViewController.cs
+++ Hello_Mac/ViewController.cs
@@ -7,15 +7,24 @@ namespace Hello_Mac
 {
 	public partial class ViewController : NSViewController
 	{
+		partial void ClickedButton(NSObject sender)
+		{
+			// Update counter and label
+			ClickedLabel.StringValue = string.Format("The button has been clicked {0} time{1}.", ++numberOfTimesClicked, (numberOfTimesClicked < 2) ? "" : "s");
+
+		}
+		private int numberOfTimesClicked = 0;
+
 		public ViewController(IntPtr handle) : base(handle)
 		{
 		}
 
 		public override void ViewDidLoad()
 		{
-			base.ViewDidLoad();
+			base.AwakeFromNib();
 
-			// Do any additional setup after loading the view.
+			// Set the initial value for the label
+			ClickedLabel.StringValue = "Button has not been clicked yet.";
 		}
 
 		public override NSObject RepresentedObject
